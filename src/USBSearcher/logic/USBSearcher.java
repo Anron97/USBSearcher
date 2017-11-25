db395e79cda16b7ec29291b49673a703429d2785
andrey <Anron2012@mail.ru>
2017-11-25 14:00:51 +0300

    Initial commit. Working application with usb, that mount in /media. Add eject usbDevice and checking of busy devices.

--- src/USBSearcher/logic/USBSearcher.java
+++ src/USBSearcher/logic/USBSearcher.java
@@ -8,9 +8,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import static java.lang.Thread.sleep;
-
-
 public class USBSearcher extends Thread{
 
     private ObservableList<USBDevice> listDevices;
